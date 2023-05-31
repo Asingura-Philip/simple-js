cdbcaf522d543c33dc8b0f5e4390170b6ef67345
Asingura-Philip <134685900+Asingura-Philip@users.noreply.github.com>
2023-05-31 19:12:06 +0300

    Add files via upload
    operator functions

---
+++ day1operators.js
@@ -0,0 +1,27 @@
+//function that takes two numbers as input and returns sum
+function minus(a, b){
+    
+    var result = a - b;
+    console.log(result);
+    
+    
+}
+function times(a, b){
+    
+    var result = a * b;
+    console.log(result);
+    
+    
+}
+function quotient(a, b){
+    
+    var result = a / b;
+    console.log(result);
+    
+    
+}
+
+
+minus(10,5);
+times(10,5);
+quotient(10,5);
