3545b39abaa716d144d4a4a531b9f69d6f70029d
Asingura-Philip <134685900+Asingura-Philip@users.noreply.github.com>
2023-06-01 14:02:27 +0300

    Add files via upload
    array length function

---
+++ day2-1.js
@@ -0,0 +1,7 @@
+//function that takes string as input and returns its length
+function long (a){
+    var arraylength = a.length;  
+    console.log(arraylength)
+}
+array = [1, 2, 3, 5];
+long(array)
