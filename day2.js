290340354883565f1768559fe09e9a56ba55a8f4
Asingura-Philip <134685900+Asingura-Philip@users.noreply.github.com>
2023-06-01 12:42:28 +0300

    Add files via upload
    js function checking odd or even

---
+++ day2.js
@@ -0,0 +1,12 @@
+//function that checks if a number is odd or even 
+
+function type(a){
+    var num = a % 2;
+    if (num == 0){
+        console.log(a + " is even");
+    }
+    else 
+    console.log(a + " is odd");
+
+}
+type(98394)
