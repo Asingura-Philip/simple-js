c3d67b2b1c32b31ac6eda6f506ac263e7b80729b
Asingura-Philip <134685900+Asingura-Philip@users.noreply.github.com>
2023-06-13 13:47:47 +0300

    functions

---
+++ day9-1.js
@@ -0,0 +1,5 @@
+//Create a program that calculates the square root of a given number
+function root(x){
+    console.log(Math.sqrt(x));
+}
+root(36);
---
+++ day9.js
@@ -0,0 +1,20 @@
+//Write a program that calculates the area of a rectangle given its length and width.
+//method 1
+function findArea(l , w){
+    let area = l * w;
+    console.log(area);
+}
+findArea(4,5);
+
+//method 2
+function area1(l , w){
+    return l * w
+    
+}
+console.log(area1(5,6));
+
+//method 3
+function area(l,w){
+    console.log(l * w);
+}
+area(8,9);
